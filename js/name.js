75e3c92507ba654329bb475c3fad2fc5ffb8f157
Ching367436 <44335786+Ching367436@users.noreply.github.com>
2020-05-21 19:46:17 +0800

    Update name.js

--- js/name.js
+++ js/name.js
@@ -1,5 +1,4 @@
-const n = document.createElement('p');
-const t = document.createTextNode('https://Ching367436.github.io');
-n.appendChild(t);
-n.style.cssText = "color:#000;opacity:.6;z-index: 9999;text-shadow: 0.2vw 0.2vw 0.2vw #fff;position:fixed;bottom:50%;right:50%;font-size:5vw;transform:translate(50%, 50%);";
-document.body.appendChild(n);
+const ching = document.createElement('p');
+ching.appendChild(document.createTextNode('https://Ching367436.github.io'));
+ching.style.cssText = "color:#000;opacity:.6;z-index: 9999;text-shadow: 0.2vw 0.2vw 0.2vw #fff;position:fixed;bottom:50%;right:50%;font-size:5vw;transform:translate(50%, 50%);";
+document.body.appendChild(ching);
